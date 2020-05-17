0cd28642a9c072710139ad1de249a462d16ea967
romefar <vlad.romanovr@gmail.com>
2020-05-17 21:23:20 +0300

    updated entry point

--- src/index.js
+++ src/index.js
@@ -1,47 +1,23 @@
-const express = require('express');
-require('./db/mongoose');
-const authMiddleWare = require('./middleware/auth');
+const express = require('express')
+const connect = require('./db/mongoose')
 
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
 
-
-
-app.listen(port, () => {
-    console.log(`Server is up on port ${port}`);
-});
-
-
-const bcrypt = require('bcryptjs');
-
-const hashTest = async () => { 
-    const pass = "saDmvs03-";
-    const hash = await bcrypt.hash(pass, 8);
-    console.log(pass);
-    console.log(hash);
-
-    const isMatch = await bcrypt.compare(pass, hash);
-    console.log(isMatch);
-    
-}
-
-const jwt = require('jsonwebtoken');
-
-const jwtTest = async () => {
-    const token = jwt.sign({ _id : '222' }, 'thisismycwcterkey', { expiresIn : '7 days' });
-    console.log(token);
-    
-    const data = jwt.verify(token, 'thisismycwcterkey');
-    console.log(data);
-    
-}
-// jwtTest();
-//hashTest();
-
+connect()
+  .then(() => {
+    console.log('\n - Succesfully connected to a database.')
+    app.listen(port, () => {
+      console.log(` -- Server is up on port ${port}`)
+    })
+  })
+  .catch(err => {
+    console.log(`An error was occured. Error: ${err.message}.`)
+  })
