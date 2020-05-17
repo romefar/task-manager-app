601536e4491c065a4b3a457f63bcee65ae2c5921
romefar <vlad.romanovr@gmail.com>
2020-05-17 21:20:08 +0300

    updated db connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/taks-manager-api', {
-    useNewUrlParser : true,
-    useUnifiedTopology : true,
-    useFindAndModify : false
-});
-mongoose.set('useCreateIndex', true);
+module.exports = () => {
+  const connect = mongoose.connect('mongodb://127.0.0.1:27017/taks-manager-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  mongoose.set('useCreateIndex', true)
 
+  return connect
+}
