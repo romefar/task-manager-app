ee6ebd33cfd9f52772750d58a9bf39d9992b6dfd
romefar <vlad.romanovr@gmail.com>
2020-05-17 21:25:50 +0300

    removed test functions

--- src/index.js
+++ src/index.js
@@ -12,36 +12,6 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 
-
-
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
-
-
-const bcrypt = require('bcryptjs');
-
-const hashTest = async () => { 
-    const pass = "saDmvs03-";
-    const hash = await bcrypt.hash(pass, 8);
-    console.log(pass);
-    console.log(hash);
-
-    const isMatch = await bcrypt.compare(pass, hash);
-    console.log(isMatch);
-    
-}
-
-const jwt = require('jsonwebtoken');
-
-const jwtTest = async () => {
-    const token = jwt.sign({ _id : '222' }, 'thisismycwcterkey', { expiresIn : '7 days' });
-    console.log(token);
-    
-    const data = jwt.verify(token, 'thisismycwcterkey');
-    console.log(data);
-    
-}
-// jwtTest();
-//hashTest();
-
